a94fb4515191c62b9557a68b8c4fad66939aed65
Baishali Datta <baishali95920@gmail.com>
2022-01-27 21:01:31 +0530

    upgrade webpack from v4 to v5

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -11,6 +11,29 @@ module.exports = {
         path: path.resolve(__dirname, '../dist')
     },
     devtool: 'source-map',
+    resolve: {
+        // modules: [...],
+        fallback: {
+            "util": false,
+            "fs": false,
+            "tls": false,
+            "net": false,
+            "path": false,
+            "zlib": false,
+            "http": false,
+            "https": false,
+            "stream": false,
+            "crypto": false,
+            "assert": false,
+            "constants": false,
+            "os": false,
+            "buffer": false,
+            "vm": false,
+            "worker_threads": false,
+            "child_process": false
+        //   "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+        } ,
+    },
     plugins:
     [
         new CopyWebpackPlugin({
